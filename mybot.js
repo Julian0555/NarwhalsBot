7853bd1b4c848499e625b582beb801e79707dbf6
Julian0555 <unconfigured@null.spigotmc.org>
2018-09-15 12:23:36 -0700

    1.1.8

--- mybot.js
+++ mybot.js
@@ -51,6 +51,9 @@ client.on("message", (message) => {
 //        message.channel.send("<:MarioHead:481325741150175232> 2");		  
 //      }
 //  }
+  if(message.member.roles.has("360923714059108353")){ // Check if member has role
+	  message.channel.send("Hi"); // Send "Hi" if he does
+  }
 });
 
 client.login(process.env.BOT_TOKEN);
